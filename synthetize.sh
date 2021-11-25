658a8bfc3f9374471303ad1988fac100644b30dc
zipotron <zipotron@gmail.com>
2021-11-25 23:48:54 +0200

    Avoid the trick of overriding top files

--- synthetize.sh
+++ synthetize.sh
@@ -1,2 +1,2 @@
-cp top/neorv32_ULX3S_BoardTop_MinimalBoot.vhd neorv32/setups/osflow/board_tops/
-make -C neorv32/setups/osflow BOARD=ULX3S MinimalBoot NEORV32_MEM_SRC_EXTRA="../../../hardware_dependent/ulx3s/wb_sdram.vhd ../../../hardware_dependent/ulx3s/wb_intercon.vhd ../../../hardware_dependent/ulx3s/ulx3s_sdram.vhd" NEORV32_VERILOG_SRC_EXTRA=../../../hardware_dependent/ulx3s/ecp5pll.sv
+
+make -C neorv32/setups/osflow BOARD=ULX3S MinimalBoot NEORV32_MEM_SRC_EXTRA="../../../hardware_dependent/ulx3s/wb_sdram.vhd ../../../hardware_dependent/ulx3s/wb_intercon.vhd ../../../hardware_dependent/ulx3s/ulx3s_sdram.vhd" NEORV32_VERILOG_SRC_EXTRA=../../../hardware_dependent/ulx3s/ecp5pll.sv DESIGN_SRC=../../../top/neorv32_ULX3S_BoardTop_Default.vhd
